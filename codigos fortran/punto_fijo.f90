bd9318b96c977abe2da2db32b7c453cc4d1bd882
Ignacio Altamira <177584654+Nachito0407@users.noreply.github.com>
2024-09-14 17:59:53 -0300

    hola

--- codigos fortran/punto_fijo.f90
+++ codigos fortran/punto_fijo.f90
@@ -1,30 +1,34 @@
 program punto_fijo
     implicit none
-    real f,x0,tolerancia,x1,g
-    integer itermax,iter,i
+    real(8) tolerancia
+    real(4) f,x1,x0
+    integer itermax,iter
 
-    tolerancia=0.000000001
-
-    itermax=30
+    tolerancia=1.0d-9
+    itermax=1000
 
     print*,'ingrese un valor de x'
     read*,x0
 
-    do
+    do iter=1 , itermax
+
         x1=f(x0)
-        iter=iter+1
-        if ( abs(x1-x0)>) then
-            
+        if (abs((x1-x0)/x1)<tolerancia) then
+            print*,'la raiz aproximada es:',x1
+            print*,'numero de iteraciones:',iter
+            exit
         end if
+    x0=x1
+
     end do  
     
+    if(iter==itermax)then
+        print*,'el metodo no converge'
+    end if
+
 end program punto_fijo
 
 function f(x)
     real x,f
-    f=4*x**3-52*x**2+160*x-100
+    f=(4*x**3-52*x**2+161*x-100)/(-1000)
 end function
-
-function g(x)
-    real g,x0
-    g=
