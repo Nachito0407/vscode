0bc346f855c117e93ddadeb8c4a86f788fba36c1
Ignacio Altamira <177584654+Nachito0407@users.noreply.github.com>
2024-09-21 20:30:24 -0300

    hola

---
+++ cod c/ejemplo9_4.c
@@ -0,0 +1,17 @@
+/* Printing floating-point numbers with  
+    floating-point conversion specifiers */
+
+#include <stdio.h>
+
+main()
+{
+    printf("%e\n", 1234567.89);
+    printf("%e\n", +1234567.89);
+    printf("%e\n", -1234567.89);
+    printf("%E\n", 1234567.89);
+    printf("%f\n", 1234567.89);
+    printf("%g\n", 1234567.89);
+    printf("%G\n", 1234567.89);
+
+    return 0;
+}
