2b4e87cdc0d669e97f006741563f07f300b70a4b
Ignacio Altamira <177584654+Nachito0407@users.noreply.github.com>
2025-02-16 13:09:29 -0300

    a

--- proyecto_raspy/include/pmodDA2.h
+++ proyecto_raspy/include/pmodDA2.h
@@ -3,3 +3,7 @@
 #define SPI_TX_BUFFER 2
 
 void DAC121S101_SendRegisterValue(unsigned short value, int spi_handler);
+
+#define SPI_TX_BUFFER 2
+
+void DAC121S101_SendRegisterValue(unsigned short value, int spi_handler);
