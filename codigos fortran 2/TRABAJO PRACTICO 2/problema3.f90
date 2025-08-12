47b610c2f05aeaaa778782d8facbadb09116c9db
Nachito0407 <Nachito0407@users.noreply.github.com>
2025-08-12 20:26:36 -0300

    practica 12/8

--- codigos fortran 2/TRABAJO PRACTICO 2/problema3.f90
+++ codigos fortran 2/TRABAJO PRACTICO 2/problema3.f90
@@ -1,4 +1,13 @@
 program factorial
+
     implicit none
+    integer i , fact, n
+    print *, "Ingrese un numero"
+    read *, n
+    fact = 1
+    do i = 2, n
+        fact = fact * i
+    end do
+    print *, "El factorial es ", fact
     
 end program factorial
